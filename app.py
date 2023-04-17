d6e456f6ab35b9f85722d8d9b889f8756ccaedc4
ribeiroAllison <allisonribeiro@gmail.com>
2023-04-17 12:13:05 -0300

    trying to set database

--- app.py
+++ app.py
@@ -1,50 +1,58 @@
+import sys
+sys.path.append('C:\\Users\\Allison Ribeiro\\coding\\flask3_db\\.venv\\Lib\\site-packages')
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warnings
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warning
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 #declaring the Book model
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key = True) #primary key column, automatically generated IDs
-    title = db.Column(db.String(80), index = True, unique = True) # book title
-    author_name = db.Column(db.String(50), index = True, unique = False) #author name
-    author_surname = db.Column(db.String(80), index = True, unique = False) #author surname
-    month = db.Column(db.String(20), index = True, unique = False) #the month of book suggestion
-    year = db.Column(db.Integer, index = True, unique = False) #the year of book suggestion
-    reviews = db.relationship('Review', backref='book', lazy='dynamic') #relationship of Books and Reviews
+    id = db.Column(db.Integer, primary_key = True) #primary key column
+    title = db.Column(db.String(80), index = True, unique = True) 
+    author_name = db.Column(db.String(50), index = True, unique = False)
+    author_surname = db.Column(db.String(80), index = True, unique = False) 
+    month = db.Column(db.String(20), index = True, unique = False) 
+    year = db.Column(db.Integer, index = True, unique = False) 
+    reviews = db.relationship('Review', backref = 'book', lazy = 'dynamic') 
     
     #Get a nice printout for Book objects
     def __repr__(self):
-        return "{} in: {},{}".format(self.title, self.month,self.year)
+        return "{} in: {},{}".format(self.title, self.month, self.year)
 
+#Declaring the Reader model
 class Reader(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), index = True, unique = False)
-    surname = db.Column(db.String(80), index = True, unique = False)
-    email = db.Column(db.String(120), index = True, unique = True)
-    reviews = db.relationship('Review', backref = 'reviewer', lazy = 'dynamic')
-
+    surname = db.Column(db.String(80), unique = False, index = True)
+    email = db.Column(db.String(120), unique = True, index = True)
+    reviews = db.relationship('Review', backref='reviewer', lazy = 'dynamic')
+  
     #get a nice printout for Reader objects
     def __repr__(self):
         return "Reader: {}".format(self.email)
-    
-    class Review(db.Model):
-        id = db.Column(db.Integer, primary_key = True) #primary key column, automatically generated IDs
-        stars = db.Column(db.Integer, unique = False) #a review's rating
-        text = db.Column(db.String(200), unique = False) #a review's text
-        book_id = db.Column(db.Integer, db.ForeignKey('book.id')) #foreign key column
-        reviewer_id = db.Column(db.Integer, db.ForeignKey('reader.id')) #foreign key column
 
-        #get a nice printout for Review objects
-        def __repr__(self):
-            return "Review: {} stars: {}".format(self.text, self.stars)
-        
+#declaring the Review model
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key = True) #primary key column, 
+    stars = db.Column(db.Integer, unique = False) #a review's rating
+    text = db.Column(db.String(200), unique = False) #a review's text
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id')) #foreign key 
+    reviewer_id = db.Column(db.Integer, db.ForeignKey('reader.id'))
 
+    #get a nice printout for Review objects
+    def __repr__(self):
+        return "Review: {} stars: {}".format(self.text, self.stars)
+
+
+#some routing for displaying the home page
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Starting App"
+    return "Start"
+
