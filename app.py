098e8fea027daf0c6b7c5b3a96367a88392d3591
ribeiroAllison <allisonribeiro@gmail.com>
2023-04-17 11:13:06 -0300

    add foreign keys to classes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warnings
 db = SQLAlchemy(app)
 
@@ -26,7 +26,23 @@ class Reader(db.Model):
     name = db.Column(db.String(50), index = True, unique = False)
     surname = db.Column(db.String(80), index = True, unique = False)
     email = db.Column(db.String(120), index = True, unique = True)
+    reviews = db.relationship('Review', backref = 'reviewer', lazy = 'dynamic')
+
+    #get a nice printout for Reader objects
+    def __repr__(self):
+        return "Reader: {}".format(self.email)
     
+    class Review(db.Model):
+        id = db.Column(db.Integer, primary_key = True) #primary key column, automatically generated IDs
+        stars = db.Column(db.Integer, unique = False) #a review's rating
+        text = db.Column(db.String(200), unique = False) #a review's text
+        book_id = db.Column(db.Integer, db.ForeignKey('book.id')) #foreign key column
+        reviewer_id = db.Column(db.Integer, db.ForeignKey('reader.id')) #foreign key column
+
+        #get a nice printout for Review objects
+        def __repr__(self):
+            return "Review: {} stars: {}".format(self.text, self.stars)
+        
 
 @app.route('/')
 @app.route('/home')
